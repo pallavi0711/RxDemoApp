1a3235e5256b7b819d8a15128f47253e04ec5a13
administrator <administrator@ntinltp0400364.local>
2018-04-03 10:35:33 +0530

    pods deleted

--- Pods/Target Support Files/Pods-TableviewPractice/Pods-TableviewPractice-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TableviewPractice : NSObject
-@end
-@implementation PodsDummy_Pods_TableviewPractice
-@end
--- Pods/Target Support Files/Pods-TableviewPracticeUITests/Pods-TableviewPracticeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TableviewPracticeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TableviewPracticeUITests
-@end
